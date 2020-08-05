d569288aa705fb0e0516ae8103d45dbd54c10dc0
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-08-05 13:22:04 +0530

    Add files via upload

--- post-micro/src/main/java/com/lti/training/postmicro/service/impl/PostServiceImpl.java
+++ post-micro/src/main/java/com/lti/training/postmicro/service/impl/PostServiceImpl.java
@@ -18,13 +18,15 @@ import com.lti.training.postmicro.dto.PostDetailListDto;
 import com.lti.training.postmicro.dto.PostDto;
 import com.lti.training.postmicro.dto.UserDetailDto;
 import com.lti.training.postmicro.entity.Post;
+import com.lti.training.postmicro.feignproxy.UserServiceProxy;
 import com.lti.training.postmicro.repository.PostRepository;
 import com.lti.training.postmicro.service.PostService;
 
 @Service
 public class PostServiceImpl implements PostService {
 
-	private static final String USER_URL = "http://localhost:9090/user-micro/user";
+	// private static final String USER_URL = "http://localhost:9090/user-micro/user";
+	private static final String USER_URL = "http://user-micro/user-micro/user";
 	
 	/*
 	@Value("${postmicro.user.url}")
@@ -33,20 +35,23 @@ public class PostServiceImpl implements PostService {
 	
 	// dependency
 	private PostRepository postRepository;
-	private RestTemplate restTemplate;
+	// private RestTemplate restTemplate;
+	private UserServiceProxy feignProxy;
 	
 	@Autowired
 	private Environment environment;
 	
 	@Autowired
-	public PostServiceImpl(PostRepository postRepository, 
-							RestTemplate restTemplate) {
+	public PostServiceImpl(PostRepository postRepository,
+						 	UserServiceProxy feignProxy) {
+							//RestTemplate restTemplate) {
 		this.postRepository = postRepository;
-		this.restTemplate = restTemplate;
+		// this.restTemplate = restTemplate;
+		this.feignProxy = feignProxy;
 		
 	}
 	
-	@Override
+	/*@Override
 	public PostDetailDto addPost(Integer userId, PostDto postDto) {
 		// Convert DTO into entity
 		Post post = new Post(null, 
@@ -98,7 +103,58 @@ public class PostServiceImpl implements PostService {
 											post.getTags(), 
 											post.getPost());
 					// return postDataDto;
-				}).collect(Collectors.toList());*/
+				}).collect(Collectors.toList());
+		List<PostDataDto> postDataList = 
+				posts.stream()
+				.map(post-> new PostDataDto(post.getId(), 
+						post.getTitle(), 
+						post.getTags(), 
+						post.getPost()))
+				.collect(Collectors.toList());
+					
+		
+		// assemble
+		PostDetailListDto postDetailListDto = 
+				new PostDetailListDto(userDetailDto, postDataList);
+		
+		return postDetailListDto;
+	}*/
+	@Override
+	public PostDetailDto addPost(Integer userId, PostDto postDto) {
+		// Convert DTO into entity
+		Post post = new Post(null, 
+							 userId, 
+							 postDto.getTitle(), 
+							 postDto.getTags(), 
+							 postDto.getPost());
+		// persist into DB
+		post = this.postRepository.save(post);
+		
+		// connect/call user-micro service
+		// String url = environment.getProperties().get("postmicro.user.url").toString();
+		UserDetailDto userDetailDto = 
+				this.feignProxy.getUserDetail(userId).getBody();
+		
+		// create DTO
+		PostDetailDto postDetailDto = 
+				new PostDetailDto(post.getId(), 
+								  post.getTitle(), 
+								  post.getTags(), 
+								  post.getPost(), 
+								  userDetailDto);
+		return postDetailDto;
+	}
+
+	@Override
+	public PostDetailListDto getAllPost(Integer userId) {
+		// get user data from user-micro
+		UserDetailDto userDetailDto = 
+				this.feignProxy.getUserDetail(userId).getBody();
+		// get data from database
+		List<Post> posts = this.postRepository.findByUserId(userId);
+		// convert all Post object(entity) into PostDataDto(DTO)
+		
+		
 		List<PostDataDto> postDataList = 
 				posts.stream()
 				.map(post-> new PostDataDto(post.getId(), 
