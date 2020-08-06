3c2aec467fba2aa68c958a6f50aec4e4135f417d
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2020-08-06 19:00:26 +0530

    Add files via upload

--- api-gateway/src/main/java/com/lti/training/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/lti/training/apigateway/security/SecurityConfig.java
@@ -4,14 +4,17 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
@@ -22,9 +25,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
 		// database : pre-defined schema
-		auth.jdbcAuthentication().dataSource(dataSource);
+		// auth.jdbcAuthentication().dataSource(dataSource);
 		
-		// in-memory credentials
+		
+		// auth.ldapAuthentication();
+		
+		// custom schema
+		auth.jdbcAuthentication().dataSource(dataSource)
+			// 	for reading username and password (authentication)
+			.usersByUsernameQuery("select username,password,enabled from users where username = ?")
+			//	for reading roles (authorization)
+			.authoritiesByUsernameQuery("select username,authority from authorities where username = ? ");
+		
+		// in-memorycredentials
 		/*
 		UserBuilder builder = User.withDefaultPasswordEncoder();
 		
@@ -43,8 +56,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	// rules to access the resource 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http
+			.cors() // allow cross origins (spring security based)
+		.and()	
+			.csrf().disable() // for rest based security
+			.authorizeRequests()  // returns an object used to configure the secured endpoints
+				.antMatchers("/post-micro/post/add").hasRole("")
+				.antMatchers(HttpMethod.DELETE).hasRole("Manager")
+				.antMatchers("/post-micro/post/**").hasAnyRole("Manager","Admin")
+				.antMatchers("/user-micro/user/**").permitAll()
+		.and() // return http object
+			.httpBasic();
 	}
 	
 	// rules additional security ( JWT / OAuth )
